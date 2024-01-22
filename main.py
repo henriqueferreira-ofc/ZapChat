9b314cf979de1b08a0bccf33bd3292b70f0ffc19
Henrique Ferreira <152978061+henriquearaujooficial@users.noreply.github.com>
2024-01-22 20:22:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# site com os scripts: https://cdnjs.com/
+# pip install python-socketio flask-socketio simple-websocket
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+# Funcionalidade de enviar mensagem
+@socketio.on("message")
+def gerenciar_mensagem(mensagem):
+    send(mensagem, broadcast=True)
+
+
+# criar a nossa 1 pagina  = 1 rota
+@app.route("/")  # decorator
+def homepage():
+    return render_template("index.html")
+
+
+# roda a nosso aplicativo
+socketio.run(app, host="192.168.1.11")
+# websocket -> tunel
